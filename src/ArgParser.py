e8d284b970ea2e40839278672082722c794b5387
Justin Morrison <morrisonxjustin@gmail.com>
2025-04-12 19:54:32 -0400

    Updated only() methods and some arguement checks to use Arg class. Also added try catch to catch ArguementErrors I throw for invalid arguements.

--- src/ArgParser.py
+++ src/ArgParser.py
@@ -83,48 +83,74 @@ class ArgParser():
         self.args = self.arg_parser.parse_args(self.argv)
 
         # Only allow one operator to be used at once
+        try:
+            if self.args.reset:
+                return self.reset_handler()
+            elif self.args.clear:
+                return self.clear_handler()
+            elif self.args.delete:
+                return self.delete_handler()
 
-        if self.args.reset:
-            return self.reset_handler()
-        elif self.args.clear:
-            return self.clear_handler()    
-        elif self.args.delete:
-            return self.delete_handler()   
-        
-        elif self.args.config_mode:
-            return self.default_mode_handler()
-        elif self.args.config_python:
-            return self.default_python_handler()
-        elif self.args.config_json:
-            return self.default_json_handler()
-        elif self.args.config:
-            return self.config_handler()
-        
-        elif self.args.python:
-            return self.python_handler()
-        elif self.args.json:
-            return self.json_handler()
-        elif self.args.text:
-            return self.text_handler()
+            elif self.args.config_mode:
+                return self.default_mode_handler()
+            elif self.args.config_python:
+                return self.default_python_handler()
+            elif self.args.config_json:
+                return self.default_json_handler()
+            elif self.args.config:
+                return self.config_handler()
 
-        elif self.only(self.args.cache):
-            return print(self.cache)
+            elif self.args.python:
+                return self.python_handler()
+            elif self.args.json:
+                return self.json_handler()
+            elif self.args.text:
+                return self.text_handler()
+
+            elif self.only(Arg.CACHE):
+                return print(self.cache)
+
+            # Handle Defaults
+            elif not any([self.args.json, self.args.python, self.args.text]):
+                mode = self.settings.default_mode
+                self.args.data = self.settings.default_map[mode]
+
+                if mode == constants.PYTHON_MODE:
+                    self.args.python = True
+                elif mode == constants.JSON_MODE:
+                    self.args.json = True
+                elif mode == constants.TEXT_MODE:
+                    self.args.text = True
+        except ArgumentError as e:
+            print(e)
+            return
+
+    def only(self, arg: Arg):
+        arg_dict = vars(self.args)
+        for key, val in arg_dict.items():  # Iterate over all of the args in the Namespace
+            if key != arg and val:
+                return False
+        return arg_dict[arg] == True
+
+    def only_iter(self, args: list[Arg]):
+        arg_dict = vars(self.args)
+        for key, val in arg_dict.items():  # Iterate over all of the args in the Namespace
+            for arg in args:
+                if key != arg and val:
+                    return False
+        return all([arg_dict[arg] for arg in args])
+
+    def only_exempt(self, arg: Arg, exempt_args: list[Arg]):
+        arg_dict = vars(self.args)
+        for key, val in arg_dict.items():  # Iterate over all of the args in the Namespace
+            if key != arg and val and key not in exempt_args:
+                return False
+        return arg_dict[arg] == True
 
 
-        # Handle Defaults
-        elif not any([self.args.json, self.args.python, self.args.text]):
-            mode = self.settings.default_mode
-            self.args.data = self.settings.default_map[mode]
-
-            if mode == constants.PYTHON_MODE:
-                self.args.python = True
-            elif mode == constants.JSON_MODE:
-                self.args.json = True
-            elif mode == constants.TEXT_MODE:
-                self.args.text = True
 
     def config_handler(self):
-        if self.only(self.args.config):
+        if self.only(Arg.CONFIG):
             print(self.settings)
 
     def default_mode_handler(self):
@@ -148,7 +174,6 @@ class ArgParser():
             self.args.data = self.settings.default_json_file
             print(self.args.data)
 
-
     def dirgen(self):
         self.files_made = self.parser.mkdir()
         self.cache.append(self.files_made)
@@ -164,31 +189,28 @@ class ArgParser():
     def python_handler(self):
         if not self.args.data:
             self.args.data = self.settings.default_python_file
+        if not Path(self.args.data).exists():
+            raise ArgumentError(self.action_map[Arg.DATA], "File does not exist")
         self.parser = PyParser(self.dst_folder, self.args.data.strip(".py"))
 
     def json_handler(self):
         if not self.args.data:
             self.args.data = self.settings.default_json_file
+        if not Path(self.args.data).exists():
+            raise ArgumentError(self.action_map[Arg.DATA], "File does not exist")
         self.parser = JSONParser(self.dst_folder, self.args.data)
 
     def text_handler(self):
         if not self.args.data:
             raise ValueError("-t: no text given")
-        self.parser = TextParser(self.dst_folder, self.args.data)
+        try:
+            self.parser = TextParser(self.dst_folder, self.args.data)
+        except JSONDecodeError as e:
+            raise ArgumentError(self.action_map[Arg.DATA], "Text not valid JSON")
 
     def reset_handler(self):
-        if self.args.cache:
-            self.cache.delete_all_files(self.args.verbose)
+        if not self.only(Arg.RESET):
+            raise ArgumentError(self.action_map[Arg.RESET], "Does not take additional arguements")
 
-        elif self.args.config:
-            self.settings.reset(self.args.verbose)
-
-        elif not self.args.config and not self.args.cache:
-            self.cache.delete_all_files(self.args.verbose)
-            self.settings.reset(self.args.verbose)
-
-
-if __name__ == "__main__":
-    app = ArgParser()
-    app.parse()
-    parser = app.get_parser()
+        self.cache.delete_all_files(self.args.verbose)
+        self.settings.reset(self.args.verbose)
